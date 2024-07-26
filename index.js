89e9b5ae705c467e869890155480bdcc648ee2c8
alexxxusachev888 <101808864+alexxxusachev888@users.noreply.github.com>
2024-07-26 09:49:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/submit', async (req, res) => {
+    const data = req.body;
+    const apiToken = (process.env.KEYCRM_API_KEY);
+    const url = 'https://openapi.keycrm.app/v1/pipelines/cards';
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiToken}`
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
